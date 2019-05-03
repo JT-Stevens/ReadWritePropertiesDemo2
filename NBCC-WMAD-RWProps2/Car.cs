d76604ea977020bd52d70c74e45fc398dfeffa36
Jeremy <Jeremy.stevnes@gmail.com>
2019-05-03 10:40:56 -0300

    Forgot to add car.cs

---
+++ NBCC-WMAD-RWProps2/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBCC_WMAD_Console
+{
+    public class Car
+    {
+        private int? speed;
+        private Nullable<decimal> price;
+        private int numberOfDoors;
+        private string carType;
+
+        public int? Year { get; set; }
+
+        public int? Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public Nullable<decimal> Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        public int NumberOfDoors
+        {
+            get
+            {
+                return numberOfDoors;
+            }
+            set
+            {
+                //Ensure the value < 10
+                if (value < 10)
+                {
+                    numberOfDoors = value;
+                }
+            }
+        }
+
+        public string CarType { get; set; }
+    }
+}
